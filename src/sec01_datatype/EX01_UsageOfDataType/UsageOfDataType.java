0359ba20691674651b2ac9b7fc1fb61ee01c2c15
jeffrey2465 <jeffrey2465@gmail.com>
2020-10-25 20:18:33 +0900

    translate tab to 4-space

--- src/sec01_datatype/EX01_UsageOfDataType/UsageOfDataType.java
+++ src/sec01_datatype/EX01_UsageOfDataType/UsageOfDataType.java
@@ -2,17 +2,12 @@ package sec01_datatype.EX01_UsageOfDataType;
 
 /*변수의 선언 및 값 대입을 위한 두 가지 방법*/
 
-public class UsageOfDataType {
-	
-	public static void main(String[] args) {
-		
-		//변수의 선언과 값의 대입을 동시에 하는 방법
-		int a = 3;
-		
-		//변수의 선언 + 값의 대입
-		int b;
-		b = 3;
-	
-	}
-	
+public class UsageOfDataType {	
+    public static void main(String[] args) {		
+        //변수의 선언과 값의 대입을 동시에 하는 방법
+        int a = 3;		
+        //변수의 선언 + 값의 대입
+        int b;
+        b = 3;	
+    }	
 }
--- src/sec01_datatype/EX03_RangeOfVariableUse/RangeOfVariableUse.java
+++ src/sec01_datatype/EX03_RangeOfVariableUse/RangeOfVariableUse.java
@@ -2,21 +2,17 @@ package sec01_datatype.EX03_RangeOfVariableUse;
 
 /*변수의 생존기간*/
 
-public class RangeOfVariableUse {
-	
-	public static void main(String[] args) {
+public class RangeOfVariableUse {	
+    public static void main(String[] args) {		
+        int value1 = 3;		
+        {
+            int value2 = 5;
+            System.out.println(value1);	//3
+            System.out.println(value2);	//5
+        }
 		
-		int value1 = 3;
-		
-		{
-			int value2 = 5;
-			System.out.println(value1); //3
-			System.out.println(value2); //5
-		}
-		
-		System.out.println(value1); //3
-		//System.out.println(value2); //오류
-		
-	}
-	
+        System.out.println(value1);		//3
+        //System.out.println(value2);	//오류		
+    }	
 }
+
